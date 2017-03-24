5e2c4370c4dff6800aa1da6bb4a561379afa1a4a
Sean Bergunder <seanbergunder@gmail.com>
2017-03-24 03:35:46 +0000

    Debug Pythonanywhere.

--- app.py
+++ app.py
@@ -21,8 +21,8 @@ def get_data():
 	
 	return df.to_json(orient='records')
 
-app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)))
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)))
     app.debug(True)
--- static/js/charts.js
+++ static/js/charts.js
@@ -2,16 +2,13 @@
 /*
  The queue() function makes sure that we have all the data transferred to the browser before drawing the graphs. 
 */
-
 queue()
     .defer(d3.json, "/data")
     .await(makeGraphs);
+
+//can use to toggle categories?
+//https://dc-js.github.io/dc.js/examples/complex-reduce.html
 	
-
-var sbc = dc.barChart("#stacked-bar-chart");
-var brusher = dc.lineChart("#brush-chart");
-var eventcount = dc.dataCount("#count");
-
 function makeGraphs(error, data) {
 	//get data from flask server
 	d3.json("/data", function(error, data) {
@@ -19,112 +16,98 @@ function makeGraphs(error, data) {
 	//datetime passed is a unix timestamp. convert to readable date
 	data.forEach(function (d) {
         d.dd= new Date(d.Datetime);
-		//console.log(d)
         d.month = d3.time.month(d.dd); // pre-calculate month for better performance
     });
 
 	var ndx = crossfilter(data);
-    var totalrecs = ndx.groupAll();
 	
-	var totalDimension = ndx.dimension(function(d) {return d;});
-	var totalGroup = totalDimension.group();
+	//define dimensions
+	var dateDim = ndx.dimension(function(d) {return d["Datetime"]; });
+	var sexDim = ndx.dimension(function(d) {return d["Gender"]; });
+	var ageDim = ndx.dimension(function(d) {return d["Age"]; });
+	var cityDim = ndx.dimension(function(d) {return d["City"]; });
+	var deadDim = ndx.dimension(function(d) {return d["Dead"]; });
+	//...
+	var allDim = ndx.dimension(function(d) {return d;});
 	
-	var yearDimension = ndx.dimension(function (d) {
-		return d3.time.year(d.dd).getFullYear();
-	});
+	var numRecordsByDate = dateDim.group();
+	var sexGroup = sexDim.group();
+	var ageGroup = ageDim.group();
+	var cityGroup = cityDim.group();
+	var deadGroup = deadDim.group();
+	//...
+	var all = ndx.groupAll();
 	
-	//group by date
-	var totalEvents = yearDimension.group().reduce(
-        // callback for when data is added to the current filter results 
-        function (p, v) {
-            ++p.total;
-			if(v.Dead == true){ ++p.deaths;}
 
-            return p;
-        },
-        // callback for when data is removed from the current filter results
-        function (p, v) {
-            --p.total;
-			if(v.Dead == true){ --p.deaths;}
-            return p;
-        },
-        // initialize p
-        function () {
-            return {
-                total: 0,
-				deaths: 0,
-            };
-        }
-    );
+	var cityDeadGroup = cityDim.group().reduce(
+		function(p, v) {
+			if(v.Dead == true){++p.fatalities;}
+			else {++p.living;}
+			++p.total;
+			return p;
+		}, function(p, v) {
+			if(v.Dead == true){--p.fatalities;}
+			else {--p.living;}
+			--p.total;
+			return p;
+		}, function() {
+			return {
+				fatalities: 0,
+				living: 0,
+				total: 0
+			};
+		});
 	
-	//full date
-	var dateDimension = ndx.dimension(function (d) {
-        return d.dd;
-    });
+	//define charts
+	var sbc = dc.barChart("#stacked-bar-chart");
+	var brusher = dc.lineChart("#brush-chart");
+	var eventcount = dc.numberDisplay("#count");
+
 	
-	var eventMonths = ndx.dimension(function (d) {
-        return d.month;
-    });
-	
-	//group by living/dead
-	var deathDimension = ndx.dimension(function(d){
-		return d.Dead;
-	});
-	
-	//group by city
-	//var cityGroup = 
-	
+	//highlights bar segment?
 	function sel_stack(i) {
 		return function(d) {
 			return d.value[i];
 		};
 	}
+
 	
+	// define chart parameters
+	eventcount
+        .formatNumber(d3.format("d"))
+		.valueAccessor(function(d) {return d;})
+        .group(all);
 	
-	eventcount // dc.dataCount('.dc-data-count', 'chartGroup');
-        .dimension(ndx)
-        .group(totalGroup)
-		.html({
-            some: '<strong>%filter-count</strong> selected out of <strong>%total-count</strong> records' +
-                ' | <a href=\'javascript:dc.filterAll(); dc.renderAll();\'>Reset All</a>',
-            all: 'All records selected. Please click on the graph to apply filters.'
-        });
-		
-		
-		
-	brusher // dc.lineChart('#monthly-move-chart', 'chartGroup')
-		.width(990)
-        .height(40)
-        .margins({top: 0, right: 50, bottom: 20, left: 40})
-        .dimension(dateDimension)
-        .group(totalGroup)
-        .x(d3.time.scale().domain([new Date(2016, 0, 1), new Date(2016, 11, 31)]))
-        .round(d3.time.month.round)
-        .xUnits(d3.time.months);
+	brusher
+		.width(650)
+		.height(140)
+		.margins({top: 10, right: 50, bottom: 20, left: 20})
+		.dimension(dateDim)
+		.group(numRecordsByDate)
+		.transitionDuration(500)
+		.x(d3.time.scale().domain([new Date(2016, 0, 1), new Date(2016, 11, 31)]))
+		.elasticY(true);
 	
 	sbc
-		.width(768)
-		.height(480)
-		.x(d3.scale.linear().domain([new Date(2016, 0, 1), new Date(2016, 11, 31)]))
-		.round(d3.time.month.round)
-		.xUnits(d3.time.months)
-        .elasticY(true)
-		.margins({left: 80, top: 20, right: 10, bottom: 20})
-		.brushOn(false)
-		.clipPadding(10)
-		.title(function(d) {
-			return d.key + '[' + this.layer + ']: ' + d.value[this.layer];
+		.width(1050)
+		.height(140)
+		.margins({top: 10, right: 50, bottom: 20, left: 20})
+		.dimension(cityDim)
+		.group(cityDeadGroup, "Living", sel_stack('1'))
+		.valueAccessor(function(d) {
+			return d.value.living;
 		})
-		.dimension(deathDimension)
-		.group(totalGroup, "1", sel_stack('1'))
-		.renderLabel(true);
+		.stack(cityDeadGroup, "Dead", function(d){return d.value.fatalities;})
+		.renderLabel(true)
+		//.rangeChart(brusher)
+		.transitionDuration(500)
+		.x(d3.scale.ordinal())
+		.xUnits(dc.units.ordinal)
+		//.elasticX(true)
+		.elasticY(true);
 	sbc.legend(dc.legend());
-	dc.override(sbc, 'legendables', function() {
-		var items = sbc._legendables();
-		return items.reverse();
-	});
-	for(var i = 2; i<6; ++i)
-		sbc.stack(totalGroup, ''+i, sel_stack(i));
+
+
 
 	dc.renderAll();
   });
